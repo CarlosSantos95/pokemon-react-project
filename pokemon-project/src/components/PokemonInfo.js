03aaeaa5161740d6485e022022bba71450d374c6
Carlos Santos <carlos.santos.carballo@gmail.com>
2022-07-22 11:37:07 +0100

    pokemon info card - wip

--- pokemon-project/src/components/PokemonInfo.js
+++ pokemon-project/src/components/PokemonInfo.js
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 import PokemonLoader from './PokemonLoader';
 import { useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
+import styles from '../styles/PokemonInfo.module.scss';
 const PokemonInfo = () => {
     // const sampleLocation = useLocation();
     // const currentPokemonUrl = sampleLocation.pathname.split("/")[1]
@@ -23,6 +24,31 @@ const PokemonInfo = () => {
                 <button onClick={() => goBack()}>back</button>
                 <h1>{currentPokemon.name}</h1>
                 {/* <PokemonCard pokemon={currentPokemon} /> */}
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6">
+                            <img src={currentPokemon.sprites.other['official-artwork'].front_default} className={`card-img-top ${styles.pokemonBg}`} alt="..." />
+                        </div>
+                        <div className="col-6">
+                            <div className="row">
+                                <div className="col-12">
+                                    Este Pokémon nace con una semilla en el lomo, que brota con el paso del tiempo.
+                                </div>
+                                <div className="col-12">
+                                    stats
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row ">
+                        <div className="col-6">
+                            PUNTOS BASE
+                        </div>
+                        <div className="col-6">
+                            TIPOS
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
